387405ca96fe2d22714dcbe7783dcd178a1bed75
Bart Nagel <bart@tremby.net>
2021-09-21 06:56:43 -0700

    Fix a grammar problem
    Tag posted by u1 tagging u2.
    Match attempt by u3 tagging u2.
    
    User u2 is common between them, and is the only common one.
    It's also possible for u3 to be the only common one.
    
    So if there's just one common, we still need both "you were" and "<user>
    was", along with "<multiple users> were".

--- lib/scoring.ts
+++ lib/scoring.ts
@@ -152,7 +152,7 @@ export async function handleMessage(game: Game, message: Message, mode: 'recount
 				console.log(`    Informing the user and deleting the message`);
 				await Promise.all([
 					(game.config.chatChannel ?? game.channel).send({
-						content: `${message.member}, you just tried to post an image ${game.config.chatChannel ? `in ${game.config.chatChannel} ` : ""}${otherAuthors.size ? `along with ${toList(otherAuthors)} ` : ""}but ${toList(commonAuthors)} were ${commonAuthors.size === 1 ? "an author" : "authors"} of the current tag. We're waiting on someone else to match it.`,
+						content: `${message.member}, you just tried to post an image ${game.config.chatChannel ? `in ${game.config.chatChannel} ` : ""}${otherAuthors.size ? `along with ${toList(otherAuthors)} ` : ""}but ${toList(commonAuthors)} ${commonAuthors.size === 1 && commonAuthors.has("you") ? "were an author" : commonAuthors.size === 1 ? "was an author" : "were authors"} of the current tag. We're waiting on someone else to match it.`,
 					}),
 					deleteMessage(message),
 				]);
