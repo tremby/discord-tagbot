d06973831be865ee6393988c68cf4921cc368350
Bart Nagel <bart@tremby.net>
2021-09-22 04:39:33 -0700

    Add more fixture helpers

--- test/fixtures.ts
+++ test/fixtures.ts
@@ -1,4 +1,4 @@
-import { Client, Guild, SnowflakeUtil, Role, TextChannel, Constants } from 'discord.js';
+import { Client, Guild, SnowflakeUtil, Role, TextChannel, Message, User, Constants } from 'discord.js';
 
 const client = new Client({ intents: [] });
 
@@ -28,9 +28,58 @@ export function getRole(guild: Guild, passedId?: string): Role {
 }
 
 export function getTextChannel(guild: Guild, id?: string): TextChannel {
-	const channel = new TextChannel(guild, {
+	return new TextChannel(guild, {
 		id: id ?? SnowflakeUtil.generate(),
 		type: Constants.ChannelTypes.GUILD_TEXT.valueOf(),
 	}, getClient());
-	return channel;
+}
+
+export function getUser(passedId?: string): User {
+	const id = passedId ?? SnowflakeUtil.generate();
+	return new User(getClient(), {
+		id,
+		username: `username-${id}`,
+		avatar: null,
+		discriminator: '1234',
+	});
+}
+
+export function getMessage(channel: TextChannel, author: User, mentions: User[], hasImage: boolean, pinned: boolean, timestamp: number | Date, content: string): Message {
+	return new Message(getClient(), {
+		id: SnowflakeUtil.generate(timestamp),
+		channel_id: channel.id,
+		guild_id: channel.guildId,
+		author: {
+			id: author.id,
+			username: author.username,
+			avatar: author.avatar,
+			discriminator: author.discriminator,
+		},
+		content,
+		timestamp: null,
+		edited_timestamp: null,
+		tts: false,
+		mention_everyone: false,
+		mentions: mentions.map((user) => ({
+			id: user.id,
+			username: user.username,
+			avatar: user.avatar,
+			discriminator: user.discriminator,
+		})),
+		mention_roles: [],
+		mention_channels: [],
+		attachments: hasImage ? [{
+			id: SnowflakeUtil.generate(),
+			filename: 'foo.jpg',
+			content_type: 'image/jpeg',
+			size: 3333,
+			url: 'http://placekitten.com/408/287',
+			proxy_url: 'http://placekitten.com/408/287',
+			height: 287,
+			width: 408,
+		}] : [],
+		embeds: [],
+		pinned,
+		type: 0,
+	});
 }
