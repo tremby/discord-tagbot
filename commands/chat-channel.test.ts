10303e80c30aab55c16a83011987dd90a4ea1e62
Bart Nagel <bart@tremby.net>
2021-12-27 21:03:20 -0800

    Add tests for chat-channel set

--- commands/chat-channel.test.ts
+++ commands/chat-channel.test.ts
@@ -12,9 +12,124 @@ import { getConfigEmbedFields } from '../lib/config';
 const guild = getGuild();
 const gameChannel = getTextChannel(guild);
 const chatChannel = getTextChannel(guild);
+const otherChannel = getTextChannel(guild);
 const user1 = getUser('user1');
 
 describe("chat-channel command", () => {
+	describe("set subcommand", () => {
+		it("responds with an error and otherwise does nothing if no channel was given", async () => {
+			const game = {
+				config: { chatChannel: null },
+			} as Game;
+			const options = [
+				{
+					type: Constants.ApplicationCommandOptionTypes.SUB_COMMAND as number, // FIXME: broken types?
+					name: 'set',
+				},
+			] as APIApplicationCommandInteractionDataOption[];
+			const interaction = getCommandInteraction(gameChannel, user1, 'chat-channel', options, {});
+			await commandSpec.handler(interaction, gameChannel, game);
+			expectInteractionResponse(interaction, true);
+			expect(game).toHaveProperty('config.chatChannel', null);
+		});
+
+		it("registers the chat channel in configuration, if previously none was set", async () => {
+			const game = {
+				config: { chatChannel: null },
+			} as Game;
+			const options = [
+				{
+					type: Constants.ApplicationCommandOptionTypes.SUB_COMMAND as number, // FIXME: broken types?
+					name: 'set',
+					options: [
+						{
+							name: 'channel',
+							type: Constants.ApplicationCommandOptionTypes.CHANNEL as number, // FIXME: broken types?
+							value: chatChannel.id,
+						},
+					],
+				},
+			] as APIApplicationCommandInteractionDataOption[];
+			const interaction = getCommandInteraction(gameChannel, user1, 'chat-channel', options, {
+				channels: { [chatChannel.id]: chatChannel },
+			});
+			await commandSpec.handler(interaction, gameChannel, game);
+			expect(game).toHaveProperty('config.chatChannel', chatChannel);
+		});
+
+		it("can switch the chat channel to another", async () => {
+			const game = {
+				config: { chatChannel: otherChannel },
+			} as Game;
+			const options = [
+				{
+					type: Constants.ApplicationCommandOptionTypes.SUB_COMMAND as number, // FIXME: broken types?
+					name: 'set',
+					options: [
+						{
+							name: 'channel',
+							type: Constants.ApplicationCommandOptionTypes.CHANNEL as number, // FIXME: broken types?
+							value: chatChannel.id,
+						},
+					],
+				},
+			] as APIApplicationCommandInteractionDataOption[];
+			const interaction = getCommandInteraction(gameChannel, user1, 'chat-channel', options, {
+				channels: { [chatChannel.id]: chatChannel },
+			});
+			await commandSpec.handler(interaction, gameChannel, game);
+			expect(game).toHaveProperty('config.chatChannel', chatChannel);
+		});
+
+		it("leaves alone any other configuration", async () => {
+			const game = {
+				config: { nextTagTimeLimit: 42, chatChannel: null },
+			} as Game;
+			const options = [
+				{
+					type: Constants.ApplicationCommandOptionTypes.SUB_COMMAND as number, // FIXME: broken types?
+					name: 'set',
+					options: [
+						{
+							name: 'channel',
+							type: Constants.ApplicationCommandOptionTypes.CHANNEL as number, // FIXME: broken types?
+							value: chatChannel.id,
+						},
+					],
+				},
+			] as APIApplicationCommandInteractionDataOption[];
+			const interaction = getCommandInteraction(gameChannel, user1, 'chat-channel', options, {
+				channels: { [chatChannel.id]: chatChannel },
+			});
+			await commandSpec.handler(interaction, gameChannel, game);
+			expect(game).toHaveProperty('config.nextTagTimeLimit', 42);
+		});
+
+		it("responds to the user", async () => {
+			const game = {
+				config: { chatChannel: null },
+			} as Game;
+			const options = [
+				{
+					type: Constants.ApplicationCommandOptionTypes.SUB_COMMAND as number, // FIXME: broken types?
+					name: 'set',
+					options: [
+						{
+							name: 'channel',
+							type: Constants.ApplicationCommandOptionTypes.CHANNEL as number, // FIXME: broken types?
+							value: chatChannel.id,
+						},
+					],
+				},
+			] as APIApplicationCommandInteractionDataOption[];
+			const interaction = getCommandInteraction(gameChannel, user1, 'chat-channel', options, {
+				channels: { [chatChannel.id]: chatChannel },
+			});
+			await commandSpec.handler(interaction, gameChannel, game);
+			expectInteractionResponse(interaction, true);
+		});
+	});
+
 	describe("unset subcommand", () => {
 		it("responds with an error and otherwise does nothing if there wasn't a chat channel already", async () => {
 			const game = {
